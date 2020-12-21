8e6b036f0658a00cb504a2c2541083fcc5a99717
mcaresano <mcaresano@gmail.com>
2020-12-21 01:29:37 -0300

    Actualizando HTML y CSS

--- vinoteca/routes/index.js
+++ vinoteca/routes/index.js
@@ -4,8 +4,13 @@ const path = require ('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join (__dirname + '/views/prueba.html'));
+  res.sendFile(path.join (__dirname + '../../views/listaProd.html'));
   //res.render('index', { title: 'Express' });
 });
+
+router.get('/detProducto', function(req, res, next) {
+  res.sendFile(path.join (__dirname + '../../views/detProducto.html'));
+});
+
 
 module.exports = router;
