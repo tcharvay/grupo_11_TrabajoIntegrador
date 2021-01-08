b87b2225b910bcd7fe98ffbe89ca39bb92bb74f9
pedroGomez17 <juanmartin17092015@gmail.com>
2021-01-07 22:43:23 -0500

    pagunina contacto y paguina ubicacion

--- vinosCatena/src/controllers/generalControllers.js
+++ vinosCatena/src/controllers/generalControllers.js
@@ -1,5 +1,17 @@
 module.exports ={
     home: function(req, res, next) {
         res.render('index') 
-    }
+    },
+    empresa: function(req, res, next) {
+        res.render('empresa') 
+    },
+    ubicacion: function(req, res, next) {
+        res.render('ubicacion') 
+    },
+    contacto: function(req, res, next) {
+        res.render('contacto') 
+    },
+    productos: function(req, res, next) {
+        res.render('productos') 
+    },
 }
--- vinosCatena/src/routers/index.js
+++ vinosCatena/src/routers/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const generalController = require('../controllers/generalControllers')
 
 router.get('/', generalController.home);
+router.get('/empresa',generalController.empresa)
+router.get('/ubicacion',generalController.ubicacion)
+router.get('/contacto',generalController.contacto)
+router.get('/productos',generalController.productos)
 
 
 module.exports = router;
