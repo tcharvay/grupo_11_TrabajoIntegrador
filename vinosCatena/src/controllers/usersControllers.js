73ee40af55ccb8874b8890863b13dd6987a45992
pedroGomez17 <juanmartin17092015@gmail.com>
2021-01-26 20:57:44 -0500

    registro de usuarios especificando eroores

--- vinosCatena/src/controllers/usersControllers.js
+++ vinosCatena/src/controllers/usersControllers.js
@@ -1,10 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const bcrypt=require ('bcrypt');
+var usuarios = fs.readFileSync(path.join(__dirname,'../data/usuarios.json'), 'utf8');
+var usuarios=JSON.parse(usuarios);
+let{ckeck,validationResult,body, check}= require('express-validator');
 module.exports ={
     login: function(req, res) {
         res.render('login') 
     },
     register: function(req, res) {
         res.render('register') 
-    }
+    },
+    create : function(req,res){
+        let errors = validationResult(req);
+        
+     if (errors.isEmpty()){
+         usuarios.push({
+         apellido :req.body.apellido,
+         nombre : req.body.nombre,    
+         email : req.body.email,
+         pasword : bcrypt.hashSync(req.body.password,12)
+        })
+        fs.writeFileSync(path.join(__dirname,'../data/usuarios.json'),JSON.stringify(usuarios));
+         res.render('login')
+     }else {
+         return res.render('register',{errors:errors.errors})
+     }
+    
+},
 }
 
 
--- vinosCatena/src/routers/users.js
+++ vinosCatena/src/routers/users.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+let{ckeck,validationResult,body, check}= require('express-validator');
 
 const usersControllers = require ('../controllers/usersControllers')
 
 router.get('/login', usersControllers.login);
 router.get('/register', usersControllers.register);
+router.post('/register',[
+    check('apellido').isLength({min:1}).withMessage('El apellido es un campo obligatorio'),
+    check('nombre').isLength({min:1}).withMessage("El nombre es un campo obligatorio"),
+    check('email').isEmail().withMessage("ingrese Email valido"),
+    check('password').isInt({min:0}).withMessage("Contraseña minimo 6 caracteres numericos").isLength({min:6}).withMessage("Contraseña minimo 6 caracteres numericos"),
+  ],usersControllers.create);
 
 module.exports = router;
